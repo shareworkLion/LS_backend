420f71eea9b4c070768158830c28c87fcc315f11
E-estrella <5eunbyeol8@gmail.com>
2022-08-05 10:13:26 +0900

    yeji_로그인, 회원가입 구현

---
+++ LetSlip/accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from django.contrib import auth
+from django.contrib.auth.models import User
+
+def login(request):
+    if request.method == "POST":
+        userid = request.POST['username']
+        pwd = request.POST['password']
+        user = auth.authenticate(request, username=userid, password=pwd)
+        if user is not None:
+            auth.login(request, user)
+            return render(request, 'home.html')
+        else:
+            return render(request, 'login.html')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password'] == request.POST['confirm']:
+            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            auth.login(request, user)
+            return render(request, 'login.html')
+    return render(request, 'signup.html')
