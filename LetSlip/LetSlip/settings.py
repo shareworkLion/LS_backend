3a2c8d1d826f7b3f5a5d91f59cf806a0363daf23
Hyeju02 <handaree02@naver.com>
2022-08-09 22:01:11 +0900

    -

--- LetSlip/LetSlip/settings.py
+++ LetSlip/LetSlip/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = mySECRET_KEY
+SECRET_KEY = 'django-insecure-l22buoa!=vhp01h)%@57$9he^mn&v#q*0yu3r*55of0!uzcfy$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
