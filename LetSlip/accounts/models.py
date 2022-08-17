801082f332f0993cfd85ac72d4e1dd0c7f5f0d01
E-estrella <5eunbyeol8@gmail.com>
2022-08-17 23:45:55 +0900

    yeji_메인 푸쉬 전

--- LetSlip/accounts/models.py
+++ LetSlip/accounts/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# # 타유저 팔로우 위한 모델
-# class FollowUser(AbstractUser):
-#     followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
-
-#     class Meta(AbstractUser.Meta):
-#         swappable = 'AUTH_USER_MODEL'
